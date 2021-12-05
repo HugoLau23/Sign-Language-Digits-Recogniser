dd3c234ee5d441a21a7fc98bce64beb706aac4c9
HugoLau23 <hugolau9145@gmail.com>
2021-12-05 20:01:39 +0800

    Original

--- VisionCoreML/ViewController.swift
+++ VisionCoreML/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var resultView: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        self.previewView.layer.addSublayer(previewLayer)
+        
+        previewLayer.frame = previewView.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
     }
-
-
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        connection.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let defaultConfig = MLModelConfiguration()
+        guard let model = try? VNCoreMLModel(for: SignLanguageDigits(configuration: defaultConfig).model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, err) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            DispatchQueue.main.sync {
+                self.resultView.text = firstObservation.identifier
+                self.confidenceLabel.text = "\(round(firstObservation.confidence * 100)) %"
+            }
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
 }
+
 
